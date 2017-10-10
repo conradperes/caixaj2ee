548d48c338d3bf77cb358ee1a1fa724c033e8269
conradperes <conradperes@conrads-mbp.hotspot.lan>
2017-10-10 10:24:24 -0300

    MUDANÇA DE VALOR DA STRING PARA O SERVIÇO DE REGISTRO DE MEMBROS

--- src/main/java/com/caixa/j2ee/webapp/controller/MemberRegistration.java
+++ src/main/java/com/caixa/j2ee/webapp/controller/MemberRegistration.java
@@ -49,6 +49,6 @@ public class MemberRegistration {
    @PostConstruct
    public void initNewMember() {
       newMember = new Member();
-      String a="banana";
+      String a="banana e maçã";
    }
 }
